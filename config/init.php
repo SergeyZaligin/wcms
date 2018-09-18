c89c6d456820c97a0d93be77c5b0c9c6eddd8a7e
Sergey Zaligin <webdevjsphp@gmail.com>
2018-09-18 18:55:31 +0300

    first commit

---
+++ config/init.php
@@ -0,0 +1,25 @@
+<?php
+
+define("DEBUG", 1);
+define("ROOT",  dirname(__DIR__));
+define("WWW",   ROOT . '/public');
+define("APP",   ROOT . '/app');
+define("CORE",  ROOT . '/vendor/wcms/core');
+define("LIBS",  ROOT . '/vendor/wcms/core/libs');
+define("CACHE", ROOT . '/tmp/cache');
+define("CONF",  ROOT . '/config');
+define("LAYOUT", 'default');
+
+// http://wcms.loc/public/index.php
+$app_path = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}";
+
+// http://wcms.loc/public/
+$app_path = preg_replace("#[^/]+$#", "", $app_path);
+
+// http://wcms.loc/
+$app_path = str_replace("/public", "", $app_path);
+
+define("PATH", $app_path);
+define("ADMIN",PATH . '/admin');
+
+require_once ROOT . "/vendor/autoload.php";
---
+++ public/index.php
@@ -0,0 +1,3 @@
+<?php
+require_once dirname(__DIR__) . '/config/init.php';
+echo $_SERVER['QUERY_STRING'];
